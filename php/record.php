8baff38471cedcb61438b2a765a19c0ad640f65c
Holden Chang <ncsm1752@naver.com>
2023-10-03 21:26:17 -0700

    deleted server data

--- php/record.php
+++ php/record.php
@@ -3,9 +3,9 @@
 $name = $_POST['username'];
 $score = $_POST['playerscores'];
 
-$serverIP = "69.172.204.200";
-$serverID = "ncsm1752_ncsm1752";
-$serverPWD = "ncsm1990cdh";
+$serverIP = "";
+$serverID = "";
+$serverPWD = "";
 $dataBaseN = "ncsm1752_tetris";
 
 $connect = mysqli_connect($serverIP, $serverID, $serverPWD, $dataBaseN);
